a7b6248c216c0409ddb5bf1bac113d9ca50afb80
Alec Xavier <72398047+alecxavier@users.noreply.github.com>
2021-07-17 06:34:15 +0800

    khan academy scrape

---
+++ main.py
@@ -0,0 +1,168 @@
+
+import os
+import random
+from re import findall
+from sys import path
+import requests
+from bs4 import BeautifulSoup
+
+os.getcwd()
+
+
+#? Algebra 1
+khan_algebra1 = requests.get('https://www.khanacademy.org/math/algebra').text
+algebra1_page = BeautifulSoup(khan_algebra1, 'lxml')
+
+
+#? Algebra 2
+khan_algebra2 = requests.get('https://www.khanacademy.org/math/algebra2').text
+algebra2_page = BeautifulSoup(khan_algebra2, 'lxml')
+
+
+main = algebra1_page.find('div', class_ = "_lhvgag5")
+for base in main:
+    # you are inside the main div file now
+    
+    for topic in main.find_all('div', class_ = "_12yy6f6l"):
+        
+        title = topic.find('h3', class_ = "_k50sd54").text
+        print(title)
+        print()
+        
+        # lesson_section = topic.find('div', class_ = "_14kisdro")
+        # print(lesson_section.text)
+        
+        for span in topic.find_all('span', class_ = "_twomc80"):
+            lesson = span.find('a', class_ = "_dwmetq").text
+            flesson = lesson[:lesson.rfind(":")]
+            
+            print(flesson)
+        
+        # for span in lesson_section.find_all('span', class_ = "_twomc80"):
+        #     lesson = span.find('a', class_ = "_dwmetq").text
+        #     print(lesson)
+        
+        print()
+        print()
+    
+    
+main = algebra2_page.find('div', class_ = "_lhvgag5")
+for base in main:
+    # you are inside the main div file now
+    
+    for topic in main.find_all('div', class_ = "_12yy6f6l"):
+        
+        title = topic.find('h3', class_ = "_k50sd54").text
+        print(title)
+        print()
+        
+        # lesson_section = topic.find('div', class_ = "_14kisdro")
+        # print(lesson_section.text)
+        
+        for span in topic.find_all('span', class_ = "_twomc80"):
+            lesson = span.find('a', class_ = "_dwmetq").text
+            flesson = lesson[:lesson.rfind(":")]
+            
+            print(flesson)
+        
+        # for span in lesson_section.find_all('span', class_ = "_twomc80"):
+        #     lesson = span.find('a', class_ = "_dwmetq").text
+        #     print(lesson)
+        
+        print()
+        print()
+    
+    
+#_______________________________
+
+
+
+def empty(input = "default"):
+    
+    site_name = input
+    first_part_site = site_name[:site_name.rindex(f"/")]
+    file_path = site_name[site_name.rindex(f"/"):]
+    
+    request_site = requests.get(f'{input}').text
+    site = BeautifulSoup(request_site, 'lxml')
+    
+    #? New section
+    
+    main = site.find('div', class_ = "_lhvgag5")
+    
+    for base in main:
+        # you are inside the main div file now
+        
+        for topic in main.find_all('div', class_ = "_12yy6f6l"):
+            
+            title = topic.find('h3', class_ = "_k50sd54").text
+            print(title)
+            print()
+            
+            # lesson_section = topic.find('div', class_ = "_14kisdro")
+            # print(lesson_section.text)
+            
+            for span in topic.find_all('span', class_ = "_twomc80"):
+                lesson = span.find('a', class_ = "_dwmetq").text
+                flesson = lesson[:lesson.rfind(":")]
+                
+                print(flesson)
+            
+            # for span in lesson_section.find_all('span', class_ = "_twomc80"):
+            #     lesson = span.find('a', class_ = "_dwmetq").text
+            #     print(lesson)
+            
+            print()
+            print()
+    
+        
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    
+url = "www.example.com"
+new_url = url[:url.rfind(".")]
+print(new_url)
+    
+    
+    
+    # for b in page.find('h3', class_ = "_k50sd54").text:
+    #     print(b)
+        
+    # for c in page.find('a', class_ = "_dwmetq").text:
+    #     print(c)
+        
+        
+        
+    # section_title = page.find_all('h3', class_ = "_k50sd54").text
+    # print(section_title)
+    
+    
+    
+    
+    
+    #insert another for loop for scraping lesson names:
+     
+        # lesson_link = page.find_all('a', class_ = "_dwmetq")
